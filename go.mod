f6a1683fbae448a0d4373ec7100a2598529a84f9
Florian Schrag <florian@schr.ag>
2020-01-18 12:01:46 +0100

    initial

---
+++ go.mod
@@ -0,0 +1,10 @@
+module api-call-protector
+
+go 1.13
+
+require (
+	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79
+	github.com/kr/http v0.0.0-20150505212737-77bd98b60462 // indirect
+	github.com/sqs/s3 v0.0.0-20150203110030-ee47412d98d9 // indirect
+	sourcegraph.com/sourcegraph/s3cache v0.0.0-20141202193749-4150cc6b0465
+)
---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"github.com/gregjones/httpcache"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
+	"time"
+)
+
+func main() {
+	target, err := url.Parse("https://eodhistoricaldata.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+	proxy := httputil.NewSingleHostReverseProxy(target)
+
+	transport := httpcache.NewTransport(httpcache.NewMemoryCache())
+	transport.Transport = NewCacheHeadersTransport()
+	proxy.Transport = transport
+
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		req.Host = req.URL.Host
+		proxy.ServeHTTP(w, req)
+	})
+
+	err = http.ListenAndServe(":8989", nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func NewCacheHeadersTransport() *CacheHeadersTransport {
+	return &CacheHeadersTransport{
+		transport: http.DefaultTransport,
+	}
+}
+
+type CacheHeadersTransport struct {
+	transport http.RoundTripper
+}
+
+func (e *CacheHeadersTransport) RoundTrip(req *http.Request) (resp *http.Response, err error) {
+	resp, err = e.transport.RoundTrip(req)
+	if err != nil {
+		return resp, err
+	}
+	if resp.StatusCode == 200 {
+		info := overWriteCacheControl(req, resp)
+
+		log.Printf("%40s: %20s - %s\n", req.URL.Path, info, req.Header.Get("cache-control"))
+	}
+	return resp, nil
+}
+
+func overWriteCacheControl(req *http.Request, resp *http.Response) string {
+	if cacheByDate(req, resp) {
+		return "cache by date"
+	}
+	if cacheMultipleDays(req, resp) {
+		return "cache multi day"
+	}
+	if cacheSingleDate(req, resp) {
+		return "cache single day"
+	}
+	return "no cache"
+}
+
+var singleDay = []string{"/exchanges/"}
+var multiDay = []string{"/bulk-fundamentals/", "/fundamentals/"}
+var olderDates = []string{"/eod-bulk-last-day/", "/eod-bulk-last-day/", "/eod/", "/div/", "/splits/"}
+
+func cacheMultipleDays(req *http.Request, resp *http.Response) bool {
+	if matchesPrefixes(multiDay, req) {
+		resp.Header.Set("cache-control", "private, max-age=1296000") // set max age to 15 days
+		return true
+	}
+	return false
+}
+
+func cacheByDate(req *http.Request, resp *http.Response) bool {
+	if matchesPrefixes(olderDates, req) {
+		dateString := req.URL.Query().Get("date")
+		if dateString != "" {
+			date, err := time.Parse("2006-01-02", dateString)
+			if err == nil {
+				if date.Before(time.Now().Add(-30 * time.Hour)) {
+					resp.Header.Set("cache-control", "private, max-age=864000") // set max age to 10 days if date is before 30 days ago
+					return true
+				}
+			}
+		}
+		resp.Header.Set("cache-control", "private, max-age=3600") // set max age to 1 hour
+		return true
+	}
+	return false
+}
+
+func cacheSingleDate(req *http.Request, resp *http.Response) bool {
+	if matchesPrefixes(singleDay, req) {
+		resp.Header.Set("cache-control", "private, max-age=86400") // set max age to 15 days
+		return true
+	}
+	return false
+
+}
+func matchesPrefixes(prefixes []string, req *http.Request) bool {
+	for _, prefix := range prefixes {
+		if strings.HasPrefix(req.URL.Path, "/api"+prefix) {
+			return true
+		}
+	}
+	return false
+}
